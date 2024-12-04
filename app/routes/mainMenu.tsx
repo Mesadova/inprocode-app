32ed792b47c6627605b10fee61fbcd6073afeec0
Mesadova <emesadova@hotmail.com>
2024-12-04 10:56:05 +0100

    Add MainMenu NavLinks

--- app/routes/mainMenu.tsx
+++ app/routes/mainMenu.tsx
@@ -1,10 +1,55 @@
-import { Outlet } from "react-router";
+import { NavLink, Outlet } from "react-router";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function MainMenu() {
     return (
-        <div>
-            <h1>Main Menu</h1>
+        <>
+            <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Brand >Flights Catalog</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                        <NavLink
+                            to="/home"
+                            style={({ isActive }) => {
+                                return isActive ? { color: "plum", textDecoration: 'none' } : {textDecoration: 'none'};
+                            }}
+                        >
+                        Home
+                        </NavLink>
+                        <NavLink
+                            to="/map"
+                            style={({ isActive }) => {
+                                return isActive ? { color: "plum", textDecoration: 'none' } : {textDecoration: 'none'};
+                            }}
+                        >
+                        Maps
+                        </NavLink>
+                        <NavLink
+                            to="/fullCalendar"
+                            style={({ isActive }) => {
+                                return isActive ? { color: "plum", textDecoration: 'none' } : {textDecoration: 'none'};
+                            }}
+                        >
+                        Full Calendar
+                        </NavLink>
+                        <NavLink
+                            to="/graphs"
+                            style={({ isActive }) => {
+                                return isActive ? { color: "plum", textDecoration: 'none' } : {textDecoration: 'none'};
+                            }}
+                        >
+                        Graphs
+                        </NavLink>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
             <Outlet />
-        </div>
+        </>
     )
 }
